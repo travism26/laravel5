820f3b93801914fc4a6a38a8d19fc0285302a160
travism26 <travismartin266@gmail.com>
2015-04-29 22:15:18 -0300

    added reactJS support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,5 +26,9 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <!-- ReactJS Support -->
+    <script src="http://fb.me/react-0.13.1.js"></script>
+    <script src="http://fb.me/JSXTransformer-0.13.1.js"></script>
 </body>
 </html>
